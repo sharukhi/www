4757b9f16738728d44ffd806edff73e1468de45e
Sharukhi <sharukhi@sharukhi.xyz>
2024-03-25 19:24:49 +0600

    fix footer

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8">
     <br>
-    <p>© Sharukhi. All rights reserved.</p>a 
+    <p>© Sharukhi. All rights reserved.</p>
   </footer>
 </template>
