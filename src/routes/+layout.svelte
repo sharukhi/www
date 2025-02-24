87caf7e7e04ec8c78d9699bf76b3e74148d2c146
sharukhi <mail@sharukhi.xyz>
2025-02-24 18:06:31 +0600

    add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,13 @@
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import Head from '$lib/components/Head.svelte';
 
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	injectSpeedInsights();
 	let { children } = $props();
 </script>
+
+<Head/>
 
 {@render children()}
