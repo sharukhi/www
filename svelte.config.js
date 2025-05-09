8d05c3ddce55a8c8b29eb40d055f0816186cf1eb
Sharukhi <mail@sharukhi.net>
2025-05-10 00:18:13 +0600

    fix routing for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		alias: {
-			"@/*": "./path/to/lib/*",
-		},
-	}
+  kit: {
+    adapter: adapter(),
+    trailingSlash: 'always',
+    alias: {
+      '@/*': './path/to/lib/*'
+    }
+  }
 };
 
 export default config;
