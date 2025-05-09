9650e668c4071f89f39fc2468b5d72c0bcb7dc3f
Sharukhi <mail@sharukhi.net>
2025-05-10 00:23:32 +0600

    fix routing

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter(),
-    trailingSlash: 'always',
-    alias: {
-      '@/*': './path/to/lib/*'
-    }
-  }
+	kit: {
+		adapter: adapter({}),
+		alias: {
+			"@/*": "./path/to/lib/*",
+		},
+	}
 };
 
 export default config;
